df7364e803406c545babf3c2b6ac2b81999237c9
Vadim Ushakov <igeekless@gmail.com>
2020-02-26 23:41:57 +0700

    Update copyrights and replace "Parcellite" back with "This program" in the GPL license notice

--- src/attr_list.h
+++ src/attr_list.h
@@ -1,13 +1,16 @@
-/* Copyright (C) 2011-2013 by rickyrockrat <gpib at rickyrockrat dot net>
+/*
+ * Rainbow CM
+ * 
+ * Copyright (C) 2015-2020 Vadim Ushakov
+ * Copyright (C) 2011-2013 by rickyrockrat <gpib at rickyrockrat dot net>
+ * Copyright (C) 2007-2008 by Xyhthyx <xyhthyx@gmail.com>
  *
- * This file is part of Parcellite.
- *
- * Parcellite is free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * Parcellite is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
